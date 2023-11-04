89031c5bc2a6c6e159f87e12fdfb3e1b5055f4bb
Diego Martinez <dmg0345@gmail.com>
2023-11-04 21:34:01 +0000

    Release 1.0.0
    - Tag files.

--- tests/tests/.test_utils/cmocka_defs.h
+++ tests/tests/.test_utils/cmocka_defs.h
@@ -1,10 +1,10 @@
 /**
  ***********************************************************************************************************************
  * @file        cmocka_defs.h
- * @author      ${PROJECT_AUTHOR} (${PROJECT_CONTACT})
- * @date        ${PROJECT_BUILD_TIMESTAMP_UTC}
- * @version     ${PROJECT_VERSION}
- * @copyright   ${PROJECT_COPYRIGHT}
+ * @author      Diego Martínez García (dmg0345@gmail.com)
+ * @date        04-11-2023 21:33:15 (UTC)
+ * @version     1.0.0
+ * @copyright   github.com/dmg0345/cb/blob/master/LICENSE
  ***********************************************************************************************************************
  */
 
